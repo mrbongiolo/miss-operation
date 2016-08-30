3b863f12323f3403ad2a954e89747874be53a7de
Ralf Schmitz Bongiolo <ralfsb2008@gmail.com>
2016-08-30 11:05:33 -0400

    Initial commit.

---
+++ lib/miss/operation.rb
@@ -0,0 +1,7 @@
+require "miss/operation/version"
+
+module Miss
+  module Operation
+    # Your code goes here...
+  end
+end
---
+++ lib/miss/operation/version.rb
@@ -0,0 +1,5 @@
+module Miss
+  module Operation
+    VERSION = "0.1.0"
+  end
+end
